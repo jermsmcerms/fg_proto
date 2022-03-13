8772a6215562f86eee7e01775ca661d32e750c2b
DESKTOP-ELQUVSM\Jerms <jeremycbrothers@gmail.com>
2022-03-13 01:31:30 -0500

    starting work on physics

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    private BoxCollider2D _pushbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
