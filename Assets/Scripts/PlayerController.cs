486845d15daa5d89e1f07b7b982d7d408d938c97
DESKTOP-ELQUVSM\Jerms <jeremycbrothers@gmail.com>
2022-03-28 13:05:52 -0400

    Making adjustments to the input buffer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,22 +16,38 @@ public class PlayerController : MonoBehaviour {
     public void OnAttack(InputAction.CallbackContext context) {
         if (context.performed) {
             _beginCharge = true;
-            if (!_fighter.PerformingAttack()) {
-                if (Mathf.Approximately(_velocity, 0.0f)) {
-                    _fighter.PerformLongPokeAttack();
-                } else {
-                    _fighter.PerformShortPokeAttack();
-                }
-            }
+            //_attackValue = 4;
+            //if (!_fighter.PerformingAttack()) {
+            //    if (Mathf.Approximately(_velocity, 0.0f)) {
+            //        _fighter.PerformLongPokeAttack();
+            //    } else {
+            //        _fighter.PerformShortPokeAttack();
+            //    }
+            //}
         } else if (context.canceled) {
             _beginCharge = false;
-            if (_fighter.IsAttackCharged() && !_fighter.PerformingAttack()) {
-                if (Mathf.Approximately(_velocity, 0.0f)) {
-                    _fighter.PerformSpecialAttack();
-                } else {
-                    _fighter.PerformInvincibleAttack();
-                }
+            //_attackValue = 0;
+            //if (_fighter.IsAttackCharged() && !_fighter.PerformingAttack()) {
+            //    if (Mathf.Approximately(_velocity, 0.0f)) {
+            //        _fighter.PerformSpecialAttack();
+            //    } else {
+            //        _fighter.PerformInvincibleAttack();
+            //    }
+            //}
+        }
+    }
+
+    float _direction;
+    public void OnMove(InputAction.CallbackContext context) {
+        if (context.performed) {
+            _direction = context.ReadValue<float>();
+            if (_direction < 0) {
+                _inputBuffer[(counter + _bufferSize) % _bufferSize][0] = 1;
+            } else if (_direction > 0) {
+                _inputBuffer[(counter + _bufferSize) % _bufferSize][0] = 2;
             }
+        } else if (context.canceled) {
+            _direction = 0.0f;
         }
     }
 
@@ -40,25 +56,40 @@ public class PlayerController : MonoBehaviour {
         _playerControls = new PlayerControls();
         _rigidbody = GetComponent<Rigidbody2D>();
         _fighter = GetComponent<Fighter>();
+
+        _inputBuffer = new int[_bufferSize][];
+        for(int i = 0; i < _inputBuffer.Length; i++) { 
+            _inputBuffer[i] = new int[_numSimultaniousInputs];
+        }
     }
 
     // Update is called once per frame
     void Update() {
-        _velocity = _playerControls.MatchControlls.Movement.ReadValue<float>() * _speed;
+        if(_fighter.IsAttackCharged()) {
+           // _attackValue = 8;
+        }
+
+        _velocity = _direction * _speed;
     }
 
-    int buffer_size = 20;
-    int[] input_buffer;
+    int _bufferSize = 60;
+    int _numSimultaniousInputs = 2;
+    int[][] _inputBuffer; // TODO: Change to list of class InputInfo that contains input and timestamp. If the input has been in the buffer too long, remove it based on its time stamp.
     int counter;
 
     static int[] leftDash = { 1, 1 };
     static int[] rightDash = { 2, 2 };
+    static int[] shortPoke = { 1, 2, 4 };
+    static int[] invincibleAttack = { 1, 2, 8 };
     private int input;
+    private int _inputDirection;
+    private int _attackValue;
 
     private bool CheckSequence(int[] sequence, int duration) {
-        int w = sequence.Length - 1;
+        int w = sequence.Length - 1;     
         for (int i = 0; i < duration; i++) {
-            int direction = input_buffer[(counter - i + buffer_size) % buffer_size];
+            int index = (counter - i + _bufferSize) % _bufferSize;
+            int direction = _inputBuffer[index][0];
             if (direction == sequence[w]) {
                 --w;
             }
@@ -66,23 +97,35 @@ public class PlayerController : MonoBehaviour {
                 return true;
             }
         }
+
         return false;
     }
 
     private void ClearInputBuffer() {
-        for (int i = 0; i < input_buffer.Length; i++) {
-            input_buffer[i] = 5;
+        for (int i = 0; i < _inputBuffer.Length; i++) {
+            _inputBuffer[i][0] = 0;
+            _inputBuffer[i][1] = 0;
         }
     }
 
     private void FixedUpdate() {
-        input_buffer[(counter + buffer_size) % buffer_size] = input;
+        _inputBuffer[(counter + _bufferSize) % _bufferSize][1] = _attackValue;
         counter++;
+
         if (_beginCharge && !_fighter.PerformingAttack()) {
             _fighter.ChargeSpecialAttack();
         }
 
+        if (CheckSequence(leftDash, 9)) {
+            Debug.Log("dash left");
+            ClearInputBuffer();
+        } else if (CheckSequence(rightDash, 9)) {
+            Debug.Log("dash right");
+            ClearInputBuffer();
+        }
+
         _rigidbody.velocity = new Vector3(_velocity, 0, 0);
+        
     }
 
     private void OnEnable() {
